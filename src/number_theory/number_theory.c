46cb0bf1583a07b4b4ac8558c86ee0e21d0bfb0a
Donghyeon Kim <donghyeon@tacocube.com>
2017-09-13 11:39:02 -0400

    Add is_prime Function

--- src/number_theory/number_theory.c
+++ src/number_theory/number_theory.c
@@ -31,3 +31,30 @@ long lcm(long a, long b) {
 	// LCM = a * b / GCD
 	return a * b / gcd(a, b);
 }
+
+long greatest_prime_factor(long num) {
+	long i;
+
+	// The greatest prime factor of a number cannot be
+	// greater than the square root of the number.
+	for (i = 2L; i * i < num; i++) {
+		if (num % i == 0L) {
+			// If i is a factor of num,
+			return greatest_prime_factor(num/i);
+		}
+	}
+	return num;
+}
+
+bool is_prime(long num) {
+	if (num == 1L) {
+		// 1 is not a prime number.
+		return false;
+	} else if (num == greatest_prime_factor(num)) {
+		// If the greatest prime factor of a number is the number itself,
+		// the number is a prime number.
+		return true;
+	} else {
+		return false;
+	}
+}
--- src/number_theory/number_theory.h
+++ src/number_theory/number_theory.h
@@ -1,5 +1,12 @@
 #ifndef NUMBER_THEORY_H
 #define NUMBER_THEORY_H
+
+#include <stdbool.h>
+
 long gcd(long, long);
+
 long lcm(long, long);
+
+bool is_prime(long);
+
 #endif // NUMBER_THEORY_H
