6052a4b612ec49cbde34e8fe985ed5156f1f9856
Donghyeon Kim <donghyeon@tacocube.com>
2017-09-13 15:56:52 -0400

    Use Sieve Method For Problem 10

--- src/10/main.c
+++ src/10/main.c
@@ -3,10 +3,23 @@
 
 int main() {
 	long sum = 0L;
-	long i;
+	int i;
+	int prime_sieve[2000000];
 
-	for (i = 0L; i < 2000000L; i++) {
-		if (is_prime(i)) sum += i;
+	for (i = 0; i < 2000000; i++) {
+		prime_sieve[i] = 1;
+	}
+
+	for (i = 1; i < 2000000; i++) {
+		if (prime_sieve[i]) {
+			if (is_prime(i)) {
+				int j;
+				for (j = i * 2; j < 2000000; j += i) {
+					prime_sieve[j] = 0;
+				}
+				sum += i;
+			}
+		}
 	}
 
 	printf("The sum of all the primes below two million is %ld\n", sum);
